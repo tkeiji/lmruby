2a8434bcf01e5d3b6dbfc26ae0bb71d86a6ec605
tkeiji <tkeiji2@gmail.com>
2012-11-29 07:49:12 +0900

    class and sort

--- hello.rb
+++ hello.rb
@@ -1,15 +1,29 @@
 #!/usr/bin/ruby
+class Bustime
+	def initialize(bust, twin)
+		@bust = bust
+		@twin = twin
+  	end
+
+	def getbust
+		@bust
+	end
+	def gettwin
+		@twin
+	end
+end
+
 t = Time.now
 puts(t.strftime("==%H:%M=="))
 lines = fileds = 0
-regbustime = Array.new();
+bustable = Array.new();
 
 file = open("regbus.txt")
 while text = file.gets do
 	textdata = text.chomp
 	textarray = textdata.split(":")
 	timedata = Time.local( t.strftime("%Y").to_i, t.strftime("%m").to_i, t.strftime("%d").to_i, textarray[0].to_i, textarray[1].to_i)
-	regbustime << timedata
+	bustable << Bustime.new(timedata, false)
 end
 file.close
 
@@ -18,9 +32,22 @@ while text = file.gets do
 	textdata = text.chomp
 	textarray = textdata.split(":")
 	timedata = Time.local( t.strftime("%Y").to_i, t.strftime("%m").to_i, t.strftime("%d").to_i, textarray[0].to_i, textarray[1].to_i)
-	regbustime << timedata
+	bustable << Bustime.new(timedata, true)
 end
 file.close
 
-p regbustime
+bussort = bustable.sort{|aa, bb|
+      aa.getbust <=> bb.getbust
+    }
 
+bussort.each{|aa|
+	tdiff = t - aa.getbust
+
+#	if(( tdiff < 600 ) && (tdiff > 1800) )
+		print aa.getbust.strftime("%H:%M")
+		if (aa.gettwin == true)
+			print " twinliner"
+		end
+		puts()
+#	end
+}
